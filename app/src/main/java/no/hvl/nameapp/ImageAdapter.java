2a030cdd52bcb15b49504b5924aa2e818eb0b164
Torbjørn <h181201@stud.hib.no>
2018-01-13 16:23:27 +0100

    Learning Mode

--- app/src/main/java/no/hvl/nameapp/ImageAdapter.java
+++ app/src/main/java/no/hvl/nameapp/ImageAdapter.java
@@ -2,7 +2,6 @@ package no.hvl.nameapp;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
--- app/src/main/java/no/hvl/nameapp/LearningModeActivity.java
+++ app/src/main/java/no/hvl/nameapp/LearningModeActivity.java
@@ -1,13 +1,99 @@
 package no.hvl.nameapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import no.hvl.nameapp.data.Person;
+import no.hvl.nameapp.data.PersonDB;
 
 public class LearningModeActivity extends AppCompatActivity {
+
+    private PersonDB db = PersonDB.getInstance();
+    private ArrayList<Person> guessed;
+    private Integer score = 0;
+    private Person currentPerson;
+    private ImageView image;
+    private TextView scoreCountView;
+    private EditText guessText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_mode);
+        startNewGame();
     }
+
+    private void startNewGame() {
+        currentPerson = db.getRandomPerson();
+        score = 0;
+        scoreCountView = findViewById(R.id.textView_score);
+        scoreCountView.setText(score.toString());
+        image = findViewById(R.id.imageView4);
+
+        image.setImageBitmap(BitmapFactory.decodeResource(getResources(),currentPerson.getPicture()));
+        guessed = new ArrayList<>();
+        guessed.add(currentPerson);
+    }
+
+    public void guess(View view) {
+        guessText = findViewById(R.id.editText_guess);
+        String guess = guessText.getText().toString();
+        if (isGuessCorrect(currentPerson, guess)) {
+            score++;
+        };
+        if(isGameover()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Game Over")
+                    .setMessage("Game over " + score + "/" + db.count())
+                    .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            startNewGame();
+                        }
+                    })
+                    .setNegativeButton("Return to Menu", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(getApplicationContext(),SelectModeActivity.class);
+                            startActivity(intent);
+                        }
+                    }).show();
+
+            startNewGame();
+        } else {
+            while(guessed.contains(currentPerson)) {
+                currentPerson = db.getRandomPerson();
+            }
+            update();
+        }
+
+
+    }
+
+    private void update() {
+        guessed.add(currentPerson);
+        image.setImageBitmap(BitmapFactory.decodeResource(getResources(),currentPerson.getPicture()));
+        scoreCountView.setText(score.toString());
+        guessText.setText("");
+    }
+
+    private boolean isGameover() {
+        return (guessed.size() >= db.count());
+    }
+
+    private boolean isGuessCorrect(Person currentPerson, String guess) {
+       return (guess.toLowerCase()).equals(currentPerson.getName().toLowerCase());
+    }
+
 }
