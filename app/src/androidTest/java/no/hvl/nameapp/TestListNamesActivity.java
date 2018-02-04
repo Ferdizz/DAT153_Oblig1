6b4b996548f97e447475f9cfa33e1792cb6f3f17
Torbjørn <h181201@stud.hib.no>
2018-02-04 17:05:10 +0100

    Fixed all tests for list names activity.

--- app/src/androidTest/java/no/hvl/nameapp/TestListNamesActivity.java
+++ app/src/androidTest/java/no/hvl/nameapp/TestListNamesActivity.java
@@ -10,9 +10,11 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.anything;
@@ -29,21 +31,44 @@ public class TestListNamesActivity {
 
 
     @Rule
-    public ActivityTestRule<ListNamesActivity> mActivityRule =
-            new ActivityTestRule(ListNamesActivity.class);
+    public ActivityTestRule<SelectModeActivity> mActivityRule =
+            new ActivityTestRule(SelectModeActivity.class);
+
 
     @Test
-    public void navigatesBack() {
-
+    public void testCorrectActivity() {
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.list_names_layout)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void clickListItem() {
+    public void testNavigateUp() {
+        onView(withId(R.id.button2)).perform(click());
+        onView(withContentDescription(R.string.abc_action_bar_up_description)).perform(click());
+        onView(withId(R.id.button)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void testNavigateBack() {
+        onView(withId(R.id.button2)).perform(click());
+        pressBack();
+        onView(withId(R.id.button)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testClickListItem() {
+        onView(withId(R.id.button2)).perform(click());
         onData(anything()).inAdapterView(withId(R.id.list_view))
                 .atPosition(0)
                 .perform(click());
         onView(withId(R.id.profileName)).check(matches(isDisplayed()));
         onView(allOf(withId(R.id.profileName), withText("Per")))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testClickAdd() {
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.pluss_button)).perform(click());
+        onView(withId(R.id.add_person_layout)).check(matches(isDisplayed()));
     }
 }
