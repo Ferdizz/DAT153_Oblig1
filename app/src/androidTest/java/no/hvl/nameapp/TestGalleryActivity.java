ecf914994d87cb9c885fb6f3dd014dfa0bca7c0a
Torbjørn <h181201@stud.hib.no>
2018-02-04 17:25:14 +0100

    Added Tests for Gallery, List and Main menu.

--- app/src/androidTest/java/no/hvl/nameapp/TestGalleryActivity.java
+++ app/src/androidTest/java/no/hvl/nameapp/TestGalleryActivity.java
@@ -20,50 +20,48 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.Matchers.allOf;
 
-
 /**
- * Created by Eier on 01.02.2018.
+ * Created by Torbjørn on 04.02.2018.
  */
-
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class TestListNamesActivity {
-
+public class TestGalleryActivity {
 
     @Rule
     public ActivityTestRule<SelectModeActivity> mActivityRule =
             new ActivityTestRule(SelectModeActivity.class);
 
 
-
     @Test
     public void testNavigateUp() {
-        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button)).perform(click());
         onView(withContentDescription(R.string.abc_action_bar_up_description)).perform(click());
         onView(withId(R.id.select_mode_layout)).check(matches(isDisplayed()));
     }
+
     @Test
     public void testNavigateBack() {
-        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button)).perform(click());
         pressBack();
         onView(withId(R.id.select_mode_layout)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testClickListItem() {
-        onView(withId(R.id.button2)).perform(click());
-        onData(anything()).inAdapterView(withId(R.id.list_view))
+        onView(withId(R.id.button)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.gridView))
                 .atPosition(0)
                 .perform(click());
         onView(withId(R.id.view_person_layout)).check(matches(isDisplayed()));
         onView(allOf(withId(R.id.profileName), withText("Per")))
                 .check(matches(isDisplayed()));
+        pressBack();
+        onView(withId(R.id.gallery_layout)).check(matches(isDisplayed()));
     }
-
 
     @Test
     public void testClickAdd() {
-        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.button)).perform(click());
         onView(withId(R.id.pluss_button)).perform(click());
         onView(withId(R.id.add_person_layout)).check(matches(isDisplayed()));
     }
--- app/src/androidTest/java/no/hvl/nameapp/TestSelectModeActivity.java
+++ app/src/androidTest/java/no/hvl/nameapp/TestSelectModeActivity.java
@@ -8,6 +8,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 
 /**
  * Created by Eier on 30.01.2018.
@@ -21,7 +27,24 @@ public class TestSelectModeActivity {
             new ActivityTestRule(SelectModeActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
+    public void testNavigateToList() {
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.list_names_layout)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void testNavigateToGallery() {
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.gallery_layout)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void testNavigateToLearningMode() {
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.learning_mode_layout)).check(matches(isDisplayed()));
 
+    }
+    @Test
+    public void testNavigateToAddPerson() {
+        onView(withId(R.id.selectImgBtn)).perform(click());
+        onView(withId(R.id.add_person_layout)).check(matches(isDisplayed()));
     }
 }
